9f8eeed5e809f2cfd3f9fa00662951bc7af47489
Gavinmi <gparzival74@gmail.com>
2015-05-26 16:32:21 -0400

    Revert "revert pls"
    This reverts commit 6dc3ec7b36bd67d3098ae80940b06311a652c862.

--- Main/hearthclicker.js
+++ Main/hearthclicker.js
@@ -44,19 +44,3 @@ function buyPack(){
         playerCards.push(cards[random])
     }
 }
-
-jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
-
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).siblings().slideUp(400);
-        jQuery('.tabs ' + currentAttrValue).delay(400).slideDown(400);
-
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-
-        e.preventDefault();
-    });
-});
-//thanks to the guy who wrote this code at http://inspirationalpixels.com/tutorials/creating-tabs-with-html-css-and-jquery
