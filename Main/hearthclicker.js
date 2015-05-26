fd1eba5fa1ae21c4fd31cd0de07420269166a03c
Gavinmi <gparzival74@gmail.com>
2015-05-26 14:34:22 -0400

    Storing Gold
    Stores amount of gold! Also a reset button

--- Main/hearthclicker.js
+++ Main/hearthclicker.js
@@ -1,9 +1,31 @@
-var gold = 0;
+var gold;
+gold = localStorage.getItem("gold");
+gold = Number(gold);
 function addGold(){
-	gold += 1;
+	if (gold == null){
+        gold = 1;
+	}
+	else{
+	    gold += 1;
+	}
 	document.getElementById("text").innerHTML = gold;
 	if (gold >= 1){
 			document.getElementById("buy").src = "images/buy_button.png";
 	}
+}
+
+function store(){
+        localStorage.setItem("gold", gold);
+ }
+
+
+function showGold(){
+    document.getElementById("text").innerHTML = localStorage.getItem("gold");
+}
+
+function reset(){
+    gold = 0;
+    localStorage.setItem("gold", 0);
+    document.getElementById("text").innerHTML = 0;
 }
 
