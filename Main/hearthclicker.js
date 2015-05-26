565ae3adce0c96f9465357c6825574348712787f
Gavinmi <gparzival74@gmail.com>
2015-05-26 16:06:44 -0400

    Random Card Generation
    Open packs; get a single card

--- Main/hearthclicker.js
+++ Main/hearthclicker.js
@@ -1,6 +1,10 @@
 var gold;
 gold = localStorage.getItem("gold");
 gold = Number(gold);
+var cards = ["Wisp", "Leper Gnome", "Undertaker","Angry Chicken"];
+var pictures = ["images/cards/wisp.png","images/cards/leper_gnome.png","images/cards/undertaker.png","images/cards/angry_chicken.png"];
+var playerCards = [];
+
 function addGold(){
 	if (gold == null){
         gold = 1;
@@ -10,14 +14,13 @@ function addGold(){
 	}
 	document.getElementById("text").innerHTML = gold;
 	if (gold >= 1){
-			document.getElementById("buy").src = "images/buy_button.png";
+		document.getElementById("buy").src = "images/buy_button.png";
 	}
 }
 
 function store(){
-        localStorage.setItem("gold", gold);
- }
-
+    localStorage.setItem("gold", gold);
+}
 
 function showGold(){
     document.getElementById("text").innerHTML = localStorage.getItem("gold");
@@ -31,3 +34,13 @@ function reset(){
     }
 }
 
+function buyPack(){
+    if(gold >= 100){
+        gold -= 100;
+        localStorage.setItem("gold", gold);
+        document.getElementById("text").innerHTML = gold;
+        var random = Math.round(Math.random() * (cards.length-1));
+        document.getElementById("card").src = pictures[random];
+        playerCards.push(cards[random])
+    }
+}
