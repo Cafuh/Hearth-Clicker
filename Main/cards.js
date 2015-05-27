c23e6c4ebf6bc7d689901e7c5eaf0169c23154b8
Gavinmi <gparzival74@gmail.com>
2015-05-27 14:03:54 -0400

    Proper Storage of cards!!!

---
+++ Main/cards.js
@@ -0,0 +1,6 @@
+var cards = [
+    wisp = {name:"Wisp", picture:"images/cards/wisp.png", weight:2},
+    leperGnome = {name:"Leper Gnome", picture:"images/cards/leper_gnome.png", weight:2},
+    undertaker = {name:"Undertaker", picture:"images/cards/undertaker.png", weight:2},
+    angryChicken = {name:"Angry Chicken", picture:"images/cards/angry_chicken.png", weight:2}
+];
